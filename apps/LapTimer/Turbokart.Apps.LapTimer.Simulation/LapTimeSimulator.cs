17a902fd0bcd2982c98116ae5a0a8929b739d138
Mads Mikkel Rasmussen <mara@aspit.dk>
2023-11-13 22:09:51 +0100

    commented the lap simulator

--- apps/LapTimer/Turbokart.Apps.LapTimer.Simulation/LapTimeSimulator.cs
+++ apps/LapTimer/Turbokart.Apps.LapTimer.Simulation/LapTimeSimulator.cs
@@ -5,18 +5,20 @@
         internal static readonly string LapTimeFormatSpecifier = @"mm\:ss\.ffff";
         private static int currentLap = 1;
         private static int counter = 1;
-        private readonly TimeSpan targetLapTime = new(0, 0, 1, 40);
+        private readonly TimeSpan targetLapTime = new(0, 0, 1, 40); // A magic number.
         public static event EventHandler CheckeredFlag;
 
         public async Task SimulateRealTime(int noOfCarts, int durationInMinutes, double simSpeed, int sessionID = 1)
         {
+            // Convert to TimeSpan so we can multiply with the simSpeed, and still use the TimeSpan:
             TimeSpan duration = TimeSpan.FromMinutes(durationInMinutes);
             duration *= simSpeed;
 
+            // A delta is the time around a specific target time. So this is plus or minus 10% the target time. 10% because to get a spread of lap times that are "just right".
             double deltaPercent = 0.10;
-            long deltaTicks = (long)((double)targetLapTime.Ticks * deltaPercent);
-            TimeSpan delta = TimeSpan.FromTicks(deltaTicks);
+            TimeSpan delta = targetLapTime * deltaPercent;
 
+            // Make karts:
             List<Kart> karts = new(noOfCarts);
             for(int i = 0; i < noOfCarts; i++)
             {
@@ -26,11 +28,16 @@
                                     delta: delta,
                                     fastness: simSpeed);
                 karts.Add(kart);
+
+                // Connect the crossed line event of each kart to the event handler:
                 kart.CrossedTheLine += OnKartCrossedTheLineAsync;
             }
 
+            // Set up parallel execution of each kart's  race:
             ParallelOptions parallelOptions = new ParallelOptions() { MaxDegreeOfParallelism = noOfCarts };
             Parallel.Invoke(new ParallelOptions() { MaxDegreeOfParallelism = 1 }, async () => await StartRaceClock(duration));
+
+            // Execute the race by calling the Race method on each kart, in parallel:
             await Parallel.ForEachAsync(
                 karts,
                 parallelOptions,
@@ -39,6 +46,7 @@
             await PrintResult(karts);
         }
 
+        // Race clock tha "counts down" until the checkered flag is flown:
         private async Task StartRaceClock(TimeSpan duration)
         {
             await Console.Out.WriteLineAsync($"{duration.ToString(LapTimeFormatSpecifier)} real time race started at [{DateTime.Now.ToString("HH:mm:ss")}]");
@@ -46,6 +54,7 @@
             CheckeredFlag?.Invoke(this, new());
         }
 
+        // Handle what happens when a kart crosses the line:
         private async void OnKartCrossedTheLineAsync(object sender, CrossedLineEventArgs e)
         {
             if(currentLap != e.Lap)
